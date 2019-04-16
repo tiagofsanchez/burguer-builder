5982cbec61f51c65452203ffbabffb47ab4acc7b
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-04-16 23:37:56 +0800

    building the buildControls piece

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css'
+
+const buildControls =(props) => { 
+    return (
+        <div className={classes.BuildControls}>
+        </div>
+    )
+}
+
+export default buildControls;
