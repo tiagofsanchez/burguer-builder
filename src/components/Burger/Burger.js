f7fed6ea85f4a3545b655883bb2b47fad139febd
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-04-16 23:11:46 +0800

    building the burger with dinamics

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,23 @@ import classes from './Burger.module.css';
 import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    const { ingredients } = props; 
+    
+    const transformedIngredients = Object.keys(ingredients)
+        .map(igKey => {
+            return [...Array(ingredients[igKey])].map((_,i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} /> ;
+            })
+        });
+  
+        
+
     return (
         <div className={classes.Burger}>
-            <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
-            <BurgerIngredient type='bread-botton' />
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-botton" />
         </div>
     );
 }
--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -3,10 +3,22 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends React.Component {
+    
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1, 
+            cheese: 2, 
+            meat: 2,
+        }
+    }
+    
     render() {
+        const { ingredients } = this.state; 
+
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients={ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
