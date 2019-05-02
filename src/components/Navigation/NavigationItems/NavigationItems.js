92c699e934e7b692a8ebdc9f2bfad4ad93844c56
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-05-02 23:37:29 +0800

    going for the sidedraw for mobile

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = (props) => (
+const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" active>Your Burger</NavigationItem>
         <NavigationItem link="/">Checkout</NavigationItem>
