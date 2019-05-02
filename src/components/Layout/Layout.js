b563d2551245aecbfd4013855e86c64a73806ebe
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-05-03 00:10:00 +0800

    working on the SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,10 +4,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar/>
+        <SideDrawer/>
         <main className ={classes.Content}>
             {props.children}
         </main>
