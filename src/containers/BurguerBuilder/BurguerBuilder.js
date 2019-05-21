25a9f457c6600060015f00af6f5b3c0c156c91a5
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-05-21 23:12:59 +0800

    starting to handle errors

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import axios from '../../axios-orders';
+
 import Aux from '../../hoc/Aux/Aux'
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+
+
 
 
 const INGREDIENT_PRICES = {
@@ -26,6 +30,7 @@ class BurgerBuilder extends React.Component {
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
+        loadingOrder: false,
     }
 
     updadtePurchasable = (ingredients) => {
@@ -93,11 +98,13 @@ class BurgerBuilder extends React.Component {
     purchaseContinueHandler = () => {
         //alert('you are about to continue!');
         //On a reall app we should recalculate the price on the server as this will be safer and the user couldn't manipulate the price
+        
+        this.setState({loadingOrder: true})
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
             customer: { 
-                name: " Tiago Sanchez ",
+                name: "Tiago Sanchez",
                 address: { 
                     street: 'you wish', 
                     zipCode: '413151',
@@ -108,8 +115,9 @@ class BurgerBuilder extends React.Component {
             deliveryMethod: 'fastest'
         }
         axios.post('/orders.json' , order)
-            .then (response => console.log(response))
-            .catch (error => console.log(error));
+            .then (response => {
+                this.setState({loadingOrder: false , purchasing: false})})
+            .catch (error => {this.setState({loadingOrder: false , purchasing: false})});   
     }
 
 
@@ -126,16 +134,23 @@ class BurgerBuilder extends React.Component {
             disableInfo[key] = disableInfo[key] <= 0
         }
 
+        //Showing the spinner if we need it
+        let oderSummary = <OrderSummary
+            ingredients={ingredients}
+            disableModel={this.purchaseHandler}
+            continuePurchase={this.purchaseContinueHandler}
+            price={totalPrice} />
+            if (this.state.loadingOrder) {
+                oderSummary = <Spinner />
+            }
+
+
         /* Will only show modal if I need. No animation */
         let modal = null;
         if (purchasing) {
             modal = (
                 <Modal disableModel={this.purchaseHandler} >
-                    <OrderSummary
-                        ingredients={ingredients}
-                        disableModel={this.purchaseHandler}
-                        continuePurchase={this.purchaseContinueHandler} 
-                        price={totalPrice}/>
+                   {oderSummary}
                 </Modal>)
         }
 
