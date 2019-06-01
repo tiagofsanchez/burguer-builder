f5c125c47b74157d886347299e4288615ab8f9e5
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-06-01 15:08:21 +0800

    Got the orders page into forms

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,42 @@ import React from 'react';
 
 import mysytle from './Order.module.css';
 
-const order = ( props ) => (
-    <div className={mysytle.Order}>
-        <p>Ingridients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div> 
-);
+const order = (props) => {
+
+    const { ingredients, price , keyToPass } = props;
+    //will transform the object into an array... there are multiple ways of doing that. 
+    //One way is in the Burger component 
+    //Another one will be here for us to work with
+
+    const newdIngredients = [];
+    for (let ingName in ingredients) {
+        newdIngredients.push(
+            {
+                name: ingName,
+                amount: ingredients[ingName],
+        })
+    }
+
+    const ingredientOutput = newdIngredients.map( ig => {
+        return <span 
+            key={ig.name}
+            style={{
+                textAlignLast:'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '8px',
+            }}
+            > {ig.name}: ({ig.amount}) </span>
+    }) 
+
+
+    return (
+            <div className={mysytle.Order}>
+                <p> Ingredients: {ingredientOutput} </p>
+                <p>Price: <strong>USD {price}</strong></p>
+            </div>
+        )
+};
 
 export default order;
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,16 +1,62 @@
 import React from 'react';
 
 import Order from '../Order/Order';
+import axios from '../../axios-orders';
+import Spinner from '../UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-class Orders extends React.Component { 
-    render () { 
+
+class Orders extends React.Component {
+
+    state = {
+        orders: [],
+        loading: true,
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(response => {
+                const fetchData = [];
+                for (let key in response.data) {
+                    fetchData.push({
+                        id:key,
+                        ...response.data[key],
+                         })
+                }
+                this.setState({ loading: false , orders: fetchData});
+                console.log(this.state.orders)
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            })
+            
+            
+    }
+
+    render() {
+
+        const { loading , orders } = this.state;
+
+        let order =
+            <div>
+                {orders.map(order => (
+                    <Order 
+                        ingredients={order.ingredients}
+                        price={+order.price}
+                        key={order.id} 
+                        />
+                ))}
+            </div>
+        if (loading) { 
+            order = <Spinner />
+        }
+
         return (
             <div>
-                <Order />
-                <Order />
+                {order}
             </div>
         )
     }
 }
 
-export default Orders; 
+export default withErrorHandler (Orders , axios); 
