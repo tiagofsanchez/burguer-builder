107454ea41121cfd3697822dc23d13e75c72d79e
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-07-09 23:00:22 +0800

    action creators and more

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import reducer from './store/reducer';
+import reducer from './store/reducers/burguerBuilder';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer);
+const store = createStore(reducer , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
